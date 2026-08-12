0c49449fc392bd9df3086ad7ccea965476a11bc3
Tracy <chipster1941@gmail.com>
2026-08-11 22:03:10 -0500

    Update headers to bypass LandWatch 403 block

--- wi_property_monitor.py
+++ wi_property_monitor.py
@@ -14,7 +14,13 @@ COUNTIES = ["barron", "polk", "dunn", "washburn"]
 MIN_ACRES = 10
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Referer": "https://www.google.com/"
 }
 
 # Craigslist Search Configs
